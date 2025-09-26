2e0826cd1633c59a6268cb287f62edf508d1f023
Igor <perekopnyyigor@gmail.com>
2025-09-26 10:25:19 +0300

    add instrument

--- src/objects/Card.js
+++ src/objects/Card.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 class Card
 {
-    async getCard(cardId) {
+     getCard(cardId) {
         const post_data = { card_id: cardId };
         axios
             .post("/index_redactor.php?action=get_card", post_data, {
                 headers: { "Content-Type": "application/json" },
             })
             .then((response) => {
-                return response.data;
+                return response;
             })
             .catch((error) => console.error(error));
     }
